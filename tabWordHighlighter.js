d118fcf1fccaf9de77bca09f8365f990277d8f4c
sheggesekta <55002220+sheggesekta@users.noreply.github.com>
2019-09-08 20:29:05 +0000

    Update tabWordHighlighter.js
    maybe this time will work

--- tabWordHighlighter.js
+++ tabWordHighlighter.js
@@ -283,6 +283,26 @@ function findWords() {
         debug&&(debugStats.findCount+=1);
 
         ReadyToFindWords = true;
+        Array.prototype.unique = function() {
+          return this.filter(function (value, index, self) { 
+            return self.indexOf(value) === index;
+          });
+        }
+
+        var links = []
+
+        var elements = document.getElementsByTagName('a')
+        for (var i = 0; i < elements.length; i++) {
+            var temp = elements[i].$("a[href*=/privnote.com/]")
+            //console.log(elements[i].getAttribute("href"));
+            //links.push(elements[i].getAttribute("href"));
+            console.log(temp);
+            links.push(temp.getAttribute("href"));
+            console.log(temp.getAttribute("href"));
+            //window.open(temp.getAttribute("href"));
+        }
+
+        console.log(links.unique());
         //}, HighlightWarmup);
     }
 
